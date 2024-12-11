7aeb03c7bc4a3becd1a2373e6aef88e209cec34f
Abhishek Dutta <22f3001838@ds.study.iitm.ac.in>
2024-12-11 11:41:48 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -10,7 +10,7 @@ def install_and_import(package):
     except ImportError:
         print(f"Package {package} not found. Installing...")
         try:
-            subprocess.check_call([sys.executable, "-m", "install", package])
+            subprocess.check_call([sys.executable, "-m", package])
         except subprocess.CalledProcessError:
             print(f"Failed to install {package}. Exiting...")
             sys.exit(1)
